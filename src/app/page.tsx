64424f90452e1ac1ab8a8f8f295bbcf7bfd4ec28
Omari Juma <jumao2669@gmail.com>
2025-07-10 22:28:08 +0300

    ft: Created basic structure for how the KCNA exam prep will be

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,72 @@
-import Image from "next/image";
-
+"use client";
+import { useState } from "react";
+import * as XLSX from "xlsx";
 export default function Home() {
+  const [file, setFile] = useState<File | null>(null);
+  const [workBookState, setWorkBookState] = useState<XLSX.WorkBook>();
+  const [selectedSheet, setSelectedSheet] = useState<String | null>(null);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold  text-amber-600">Study Like a Pro</h1>
+      <strong className="text-2xl  italic text-amber-500">
+        "Upload your exam questions and answers and get AI-generated summaries"
+      </strong>
+      <div className="mt-10 flex flex-col items-center gap-5">
+        <input
+          type="file"
+          className="hidden"
+          name="file-upload"
+          id="file-upload"
+          accept=".xls, .xlsx"
+          onChange={(e) => {
+            const file = e.target.files?.[0];
+            if (file) {
+              setFile(file);
+              const reader = new FileReader();
+              reader.onload = (e) => {
+                const data = e.target?.result;
+                if (data) {
+                  const workBook = XLSX.read(data, { type: "array" });
+                  setWorkBookState(workBook);
+                  console.log(workBook);
+                }
+              };
+              reader.onerror = (e) => {
+                console.log(e);
+              };
+              reader.readAsArrayBuffer(file);
+            }
+          }}
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <label
+          htmlFor="file-upload"
+          className="cursor-pointer text-amber-600 bg-gray-50 px-4 py-2 rounded-md"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          Upload your exam questions and answers
+        </label>
+        {file && (
+          <p className="bg-amber-500 text-white px-4 py-2 rounded-md w-1/3">
+            {file.name} <span className="text-white">{file.size} bytes</span>
+          </p>
+        )}
+        {workBookState && (
+          <div className="mt-4 flex flex-wrap gap-3 justify-center">
+            {workBookState.SheetNames.map((sheetName) => (
+              <button
+                key={sheetName}
+                className={`px-4 py-2 rounded-full font-bold transition-all ${
+                  selectedSheet === sheetName
+                    ? "bg-amber-600 text-white scale-110"
+                    : "bg-amber-200 text-amber-800 hover:bg-amber-400"
+                }`}
+                onClick={() => setSelectedSheet(sheetName)}
+              >
+                {sheetName}
+              </button>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
